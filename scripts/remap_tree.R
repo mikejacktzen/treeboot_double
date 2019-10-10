05f7d38e6daabbd5bc62b03acfeba9704512524f
statsccpr <statsccpr@users.noreply.github.com>
2019-10-10 12:09:32 -0700

    Create remap_tree.R

---
+++ scripts/remap_tree.R
@@ -0,0 +1,9 @@
+# to do a 2nd level treebootstrap with RDStreeboot:::.TBS()
+# need to puzzle/figure out how to recreate tree / adjacency
+# for each b, id_boot_1_tree = create_tree(id_boot_0[[b]])
+
+# key step is to match descendent ids to ancestor ids
+
+remap_tree = function(id_boot_desc,table_edges_ances){
+
+}
