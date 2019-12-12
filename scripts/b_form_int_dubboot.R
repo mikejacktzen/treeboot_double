e033bd82f7a8d47f5b1b3d8b6aa14286e09bbd5d
Michael Tzen <mtzen@ccpr.ucla.edu>
2019-12-12 13:56:38 -0800

    #bugfix
    * sort() instead of quantile()

--- scripts/b_form_int_dubboot.R
+++ scripts/b_form_int_dubboot.R
@@ -15,20 +15,16 @@ form_int_dubboot = function(df_est_samp_orig,
 	# u_ref = B1_na*quantile(df_ref_quants$u_b,(alpha/2)*B1_na*1e-2)
 	B1_na = nrow(df_ref_quants)
 	
-	l_ref = B1_na*quantile(df_ref_quants$u_b,(1-alpha/2))
-	u_ref = B1_na*quantile(df_ref_quants$u_b,(alpha/2))
-	
-	# floor(l_ref)
-	# floor(u_ref)
-	
-	# 1:5
-	# (1:5)[5]
-	# (1:5)[3]
-	# (1:5)[1]
+	l_ref = B1_na*sort(df_ref_quants$u_b)[B1_na*(1-alpha/2)]
+	u_ref = B1_na*sort(df_ref_quants$u_b)[B1_na*(alpha/2)]
 	
 	# using above limits to lookup ordered roots of first level bootstrap estimates
-	l_ref_root = df_ref_quants$root_pivot_0_b[floor(l_ref)]
-	u_ref_root = df_ref_quants$root_pivot_0_b[ifelse(floor(u_ref)==0,1,floor(u_ref))]
+	root_pivot_0_b_sorted = sort(df_ref_quants$root_pivot_0_b)
+	
+	# using above limits to lookup ordered roots of first level bootstrap estimates
+	
+	l_ref_root = root_pivot_0_b_sorted[floor(l_ref)]
+	u_ref_root = root_pivot_0_b_sorted[ifelse(floor(u_ref)==0,1,floor(u_ref))]
 	
 	
 	# df_est_samp_orig = compute_quants_possibly(df_use)
@@ -36,8 +32,8 @@ form_int_dubboot = function(df_est_samp_orig,
 	# ll_dtbs_pivot = df_est_samp_orig$est_samp - l_ref_root*df_est_samp_orig$se_samp
 	# ul_dtbs_pivot = df_est_samp_orig$est_samp - u_ref_root*df_est_samp_orig$se_samp
 	
-	ll_dtbs_pivot = df_est_samp_orig[1] - l_ref_root*df_est_samp_orig[2]
-	ul_dtbs_pivot = df_est_samp_orig[1] - u_ref_root*df_est_samp_orig[2]
+	ll_dtbs_pivot = as.numeric(df_est_samp_orig[1] - l_ref_root*df_est_samp_orig[2])
+	ul_dtbs_pivot = as.numeric(df_est_samp_orig[1] - u_ref_root*df_est_samp_orig[2])
 	
 	if(ul_dtbs_pivot >= ll_dtbs_pivot){
 		ul=ul_dtbs_pivot
@@ -51,7 +47,7 @@ form_int_dubboot = function(df_est_samp_orig,
 		# mean_est_b = mean(df_ref_quants$est_b),
 		length_int = abs(ul_dtbs_pivot-ll_dtbs_pivot),
 		# ll_dtbs_pivot,ul_dtbs_pivot,
-		ll,ul
+		ll=ll,ul=ul
 		)
 	
 	return(est_dubboot_out)
