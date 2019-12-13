ca6f20574d64f6d48133ea6329035d95e3a73d13
Michael Tzen <mtzen@ccpr.ucla.edu>
2019-12-12 17:00:18 -0800

    switch to quantile() in interval instead of manually looking up sort()

--- scripts/b_form_int_dubboot.R
+++ scripts/b_form_int_dubboot.R
@@ -3,37 +3,26 @@
 form_int_dubboot = function(df_est_samp_orig,
 														df_ref_quants,alpha=0.05){
 	
-	# alpha=0.05
 	
-	# https://pdfs.semanticscholar.org/46b5/2cb68dc22b1293da76b46b66ae1629ea8a43.pdf
-	
-	# all the effort is to obtain reference distribution
-	# roots 'l_ref' and 'l_ref_root
-	
-	# limits of pivotal reference distribution
-	# l_ref = B1_na*quantile(df_ref_quants$u_b,(1-alpha/2)*B1_na*1e-2)
-	# u_ref = B1_na*quantile(df_ref_quants$u_b,(alpha/2)*B1_na*1e-2)
-	B1_na = nrow(df_ref_quants)
-	
-	l_ref = B1_na*sort(df_ref_quants$u_b)[B1_na*(1-alpha/2)]
-	u_ref = B1_na*sort(df_ref_quants$u_b)[B1_na*(alpha/2)]
-	
-	# using above limits to lookup ordered roots of first level bootstrap estimates
 	root_pivot_0_b_sorted = sort(df_ref_quants$root_pivot_0_b)
 	
 	# using above limits to lookup ordered roots of first level bootstrap estimates
 	
-	l_ref_root = root_pivot_0_b_sorted[floor(l_ref)]
-	u_ref_root = root_pivot_0_b_sorted[ifelse(floor(u_ref)==0,1,floor(u_ref))]
+	# l_ref_root = root_pivot_0_b_sorted[floor(l_ref)]
+	# u_ref_root = root_pivot_0_b_sorted[ifelse(floor(u_ref)==0,1,floor(u_ref))]
 	
+	# above is manual
+	# same as using quantile()
+	l_ref_root = quantile(root_pivot_0_b_sorted,(1-alpha/2))
+	u_ref_root = quantile(root_pivot_0_b_sorted,(alpha/2))
 	
 	# df_est_samp_orig = compute_quants_possibly(df_use)
 	
 	# ll_dtbs_pivot = df_est_samp_orig$est_samp - l_ref_root*df_est_samp_orig$se_samp
 	# ul_dtbs_pivot = df_est_samp_orig$est_samp - u_ref_root*df_est_samp_orig$se_samp
 	
-	ll_dtbs_pivot = as.numeric(df_est_samp_orig[1] - l_ref_root*df_est_samp_orig[2])
-	ul_dtbs_pivot = as.numeric(df_est_samp_orig[1] - u_ref_root*df_est_samp_orig[2])
+	ll_dtbs_pivot = as.numeric(df_est_samp_orig[[1]] - l_ref_root*df_est_samp_orig[[2]])
+	ul_dtbs_pivot = as.numeric(df_est_samp_orig[[1]] - u_ref_root*df_est_samp_orig[[2]])
 	
 	if(ul_dtbs_pivot >= ll_dtbs_pivot){
 		ul=ul_dtbs_pivot
